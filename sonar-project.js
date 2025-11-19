9d87de1422439acf9a5692ecb28cc5fef7c52ae9
fegisucepto <fegisucepto@gmail.com>
2025-11-19 12:12:32 +0700

    Fix SonarQube configuration with token and disable auto-analysis

--- sonar-project.js
+++ sonar-project.js
@@ -3,6 +3,7 @@ const scanner = require('sonarqube-scanner').default;
 scanner(
   {
     serverUrl: 'https://sonarcloud.io',
+    token: process.env.SONAR_TOKEN,  // Gunakan token dari environment variable
     options: {
       'sonar.organization': 'fegisucepto',
       'sonar.projectKey': 'fegisucepto_belajar-nodejs-dasar',
@@ -12,9 +13,10 @@ scanner(
       'sonar.exclusions': 'node_modules/**,**/*.test.js,**/*.spec.js,coverage/**,dist/**,**/__tests__/**,**/test/**',
       'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
       'sonar.testExecutionReportPaths': 'test-report.xml',
-      'sonar.scm.disabled': 'true',  // Nonaktifkan SCM jika tidak diperlukan
-      'sonar.scm.provider': 'git',    // Atau 'svn' jika menggunakan SVN
-      'sonar.scm.exclusions.disabled': 'true'  // Nonaktifkan pengecualian SCM
+      'sonar.scm.disabled': 'true',
+      'sonar.scm.provider': 'git',
+      'sonar.scm.exclusions.disabled': 'true',
+      'sonar.qualitygate.wait': 'true'  // Tunggu hasil quality gate
     },
   },
   () => process.exit()
