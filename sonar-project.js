30fe96e4c47b7f392e7e3089cc82eccfbbfdd30a
fegisucepto <fegisucepto@gmail.com>
2025-11-19 12:04:12 +0700

    Fix SonarQube scanner configuration

--- sonar-project.js
+++ sonar-project.js
@@ -1,18 +1,20 @@
-const { scanner } = require('sonarqube-scanner');
+const scanner = require('sonarqube-scanner').default;
 
 scanner(
   {
     serverUrl: 'https://sonarcloud.io',
     options: {
-      'sonar.organization': 'your-organization-key', // Ganti dengan organization key Anda
-      'sonar.projectKey': 'your-project-key',        // Ganti dengan project key unik
-      'sonar.projectName': 'Nama Project Anda',
+      'sonar.organization': 'fegisucepto', // Ganti dengan organization key Anda
+      'sonar.projectKey': 'fegisucepto_belajar-nodejs-dasar', // Ganti dengan project key unik
+      'sonar.projectName': 'belajar-nodejs-dasar',
       'sonar.sources': '.',
       'sonar.tests': '__tests__',
       'sonar.test.inclusions': '**/*.test.js,**/*.spec.js',
       'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
       'sonar.sourceEncoding': 'UTF-8',
-      'sonar.exclusions': 'node_modules/**,**/*.test.js,**/*.spec.js,coverage/**,dist/**',
+      'sonar.exclusions': 'node_modules/**,**/*.test.js,**/*.spec.js,coverage/**,dist/**,**/__tests__/**',
+      'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
+      'sonar.testExecutionReportPaths': 'test-report.xml'
     },
   },
   () => process.exit()
